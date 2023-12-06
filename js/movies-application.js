0583a80514edb5a82b1099cab5ad8796bb8151af
Chanee Teriipaia-Molett <chanee.teriipaiamolett@gmail.com>
2023-12-06 16:57:39 -0600

    feat:fixing.

--- js/movies-application.js
+++ js/movies-application.js
@@ -1,39 +1,44 @@
 "user strict";
-document.addEventListener('DOMContentLoaded', () => {
-  const loadingDiv = document.getElementById('loading');
+(() => {
+    fecth('http://localhost:3000/movies').then(resp => resp.json()).then(data => console.log(data));
+    fetch('http://localhost:3000/movies/4').then(resp => resp.json()).then(data => fetch('http://localhost:3000/actors/' + data.actorId)).then(resp => resp.json()).then(data => console.log(data));
 
-  fetch('http://localhost:3000/movies')
-    .then(response => response.json())
-    .then(movies => {
-      // Replace loading message with HTML generated from movies
-      loadingDiv.style.display = 'none';
-      // Implement code to display movies on the page
-    })
-    .catch(error => console.error(error));
-});
-document.addEventListener('DOMContentLoaded', () => {
-    // ... (existing code)
+    document.addEventListener('DOMContentLoaded', () => {
+        const loadingDiv = document.getElementById('loading');
 
-    const addMovieForm = document.getElementById('addMovieForm');
-
-    addMovieForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-
-        const title = document.getElementById('title').value;
-        const rating = document.getElementById('rating').value;
-
-        fetch('http://localhost:3000/movies', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ title, rating }),
-        })
+        fetch('http://localhost:3000/movies')
             .then(response => response.json())
-            .then(newMovie => {
-                // Handle the newly added movie
-                console.log('New movie added:', newMovie);
+            .then(movies => {
+                // Replace loading message with HTML generated from movies
+                loadingDiv.style.display = 'none';
+                // Implement code to display movies on the page
             })
             .catch(error => console.error(error));
     });
-});
+    document.addEventListener('DOMContentLoaded', () => {
+        // ... (existing code)
+
+        const addMovieForm = document.getElementById('addMovieForm');
+
+        addMovieForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            const title = document.getElementById('title').value;
+            const rating = document.getElementById('rating').value;
+
+            fetch('http://localhost:3000/movies', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({title, rating}),
+            })
+                .then(response => response.json())
+                .then(newMovie => {
+                    // Handle the newly added movie
+                    console.log('New movie added:', newMovie);
+                })
+                .catch(error => console.error(error));
+        });
+    });
+})();
